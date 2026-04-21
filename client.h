af79b151496f6f71e0b337681fe66563579c1f30
wnghks7787 <wnghks7787@naver.com>
2026-04-21 15:08:43 +0900

    ready to connect TCP

---
+++ client.h
@@ -0,0 +1,30 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QObject>
+#include <QtNetwork/QTcpSocket>
+
+class Client : QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Client(QObject *parent = nullptr);
+
+    void connectToServer(QString host = "127.0.0.1", int port = 1234);
+    void sendLogin(QString id, QString pw);
+
+signals:
+    void connected();
+    void disconnected();
+
+private slots:
+    void onConnected();
+    void onReadyRead();
+    void onDisconnected();
+
+private:
+    QTcpSocket* socket;
+};
+
+#endif // CLIENT_H
