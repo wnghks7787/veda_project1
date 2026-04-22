bd09d821c6535a226d2f64d0560db5a8f961e8e7
wnghks7787 <wnghks7787@naver.com>
2026-04-22 10:23:39 +0900

    update

--- qt_project/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/deploy_qt_project_33b3eeb19f.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/3-10/Desktop/qt_project/veda_project1_new/qt_project/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/qt_project-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/3-10/Desktop/qt_project/veda_project1_new/qt_project/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/qt_project.exe"
-    GENERATE_QT_CONF
-)
